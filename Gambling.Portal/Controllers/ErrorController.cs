eed1d373e3954b7d579e2045de5efdfe2d6e6075
danielhorlescu <thunder_332002@yahoo.com>
2015-09-03 21:08:01 +1000

    Added Portal project using MVC Skeleton online project template

---
+++ Gambling.Portal/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Gambling.Portal.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Http404()
+        {
+            Response.StatusCode = 404;
+
+            return View();
+        }
+    }
+}
---
+++ Gambling.Portal/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Gambling.Persistance;
+
+namespace Gambling.Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFileRepository _fileRepository;
+
+        public HomeController(IFileRepository fileRepository)
+        {
+            _fileRepository = fileRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
